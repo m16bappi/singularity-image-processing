061b4a63f9e3ca8afddf563e7bcce395fe09ed7a
Mehedi Hasan Bappi <mehedibubt34@gmail.com>
2025-02-21 20:51:19 +0600

    inital commit

--- routes/image.py
+++ routes/image.py
@@ -3,16 +3,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("")
+@router.post("upload")
 async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
-
-
-@router.get("/me")
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
-
-
-@router.get("/{username}")
-async def read_user(username: str):
-    return {"username": username}
+    return {"success": True}
